6f31803928ed5b10dc9f4cc9bb3643416a499e5d
Chris Wainwright <cjwainwright@cantab.net>
2019-11-16 17:24:45 +0000

    Allowing tests to be run by specifying value in process args (e.g. set in launch.json in vscode)

--- test/run.js
+++ test/run.js
@@ -1,9 +1,12 @@
 const build = require('../src/index');
 
+const file = process.argv[2];
+console.log(`Running tests for '${file}'`);
+
 // run a specific test for debugging
 (async () => {
     await build(config => { 
-        config.input = './test/test1/src'
-        config.output = './test/test1/dist'
+        config.input = `./test/${file}/src`
+        config.output = `./test/${file}/dist`
     });
 })();
