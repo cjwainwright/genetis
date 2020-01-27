b64b52bd02f2939cd1987be4a228b525a59fe6f6
Chris Wainwright <cjwainwright@cantab.net>
2020-01-27 23:01:09 +0000

    Allowing a config file

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const build = require('./index');
 
-(async () => {
-    await build(config => {
-    });
-})();
+const configFile = path.join(process.cwd(), 'ssg.conf.js');
+fs.exists(configFile, exists => {
+    const config = exists ? require(configFile): c => {};
+    (async () => {
+        await build(config);
+    })();
+});
+
