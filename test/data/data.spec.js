cfa0261f5dda5832a72a209ae94f5bff51e753f9
Chris Wainwright <cjwainwright@cantab.net>
2020-02-15 12:46:53 +0000

    Testing with a failing test

--- test/data/data.spec.js
+++ test/data/data.spec.js
@@ -14,6 +14,8 @@ describe('data', () => {
         });
     });
 
+it('fails', () => { expect(true).toBe(false); });
+
     it('creates an html file for each page', async () => {
         let stats = await fs.promises.stat(path.join(outputDir, 'page1.html'));
         expect(stats.isFile()).toBe(true);
